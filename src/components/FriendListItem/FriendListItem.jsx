1192eaf8b0d4cb2d1e83428bdd25c8344e7fef8e
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-06-17 16:02:15 +0300

    add code task2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,13 @@
 import css from "./FriendListItem.module.css";
 
-const FriendListItem = ({friend:{avatar, name, isOnline }}) => {
+const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+    
     return (
         <div className={css.container}>
             <img className={css.images} src={avatar} alt="Avatar" width="48" />
             <p className={css.textName}>{name}</p>
-            <p className={css.textStatus}>{isOnline}</p>    
+            {isOnline === true?<p className={css.textStatusOn}>Online</p>:<p className={css.textStatusOff}>Offline</p> }
+              
 </div>
 
     )
